e508b70b3dfb52fa982fa7925b14d91fa1773102
Shaarif <shaarifshahid@gmail.com>
2022-10-02 15:54:54 +0500

    Jest Unit Test

---
+++ timesTwo.js
@@ -0,0 +1,6 @@
+// Task 1: Code the timesTwo function declaration
+const timesTwo = (number) => {
+    return 2 * number;
+}
+// Task 2: Export the timesTwo function as a module
+module.exports = timesTwo;
